35ae3e0d96c3e5e5fa048fcde7bda06f51e5cc40
Gopi200 <61714236+Gopi200@users.noreply.github.com>
2024-05-30 16:11:35 +0200

    Committing for test

--- Turtlefiles_new/setup.lua
+++ Turtlefiles_new/setup.lua
@@ -1,3 +1,18 @@
-for i = 1, #arg do
-    print(arg[i])
+function update_drive()
+
 end
+
+if arg[3] == "install" then
+    settings.define("Turtlejs_version", {
+        type = "string"
+    })
+
+end
+
+if arg[3] == "update" then
+    if arg[4] == "drive" then
+        update_drive()
+    end
+end
+
+-- arg[3] is the actual argument
